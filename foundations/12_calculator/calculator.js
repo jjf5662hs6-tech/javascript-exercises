6191863ff67ce96732841af761f98601faf50445
jjf5662hs6-tech <250735204+jjf5662hs6-tech@users.noreply.github.com>
2026-02-03 18:56:49 -0500

    completed exercises but factorials need more studying

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	let result = num1 + num2;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	let result = num1 - num2;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+
+	let total = 0;
+  for (let number of numbers){
+    total = total + number;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1;
+  for (let number of array){
+    total = total * number;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num, exponentNum) {
+	result = num ** exponentNum;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0){
+    return 1;
+  }
+	let result = 1;
+  for(let i = 2; i <= num; i++) {
+    result = result * i;
+  }
+  return result;
 };
 
 // Do not edit below this line
