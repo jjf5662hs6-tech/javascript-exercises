2d90bffa2d810a0ee98bb276c3567b0fec9a1a5a
jjf5662hs6-tech <250735204+jjf5662hs6-tech@users.noreply.github.com>
2026-02-03 22:19:21 -0500

    learned these methods

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let cleaned = string.toLowerCase()
+                        .replace(/[^a-zA-Z0-9]/gi, "");
 
+    let reversedString = cleaned.split("")
+                                .reverse()
+                                .join("");
+
+    return cleaned === reversedString;
 };
 
 // Do not edit below this line
