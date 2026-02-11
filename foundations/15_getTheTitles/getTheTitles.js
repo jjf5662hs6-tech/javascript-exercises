1016182e4fe80d2f3467fba96eb686927bc14564
jjf5662hs6-tech <250735204+jjf5662hs6-tech@users.noreply.github.com>
2026-02-11 16:34:05 -0500

    finished this, it was an easier one for me

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let mappedArray = array.map(obj => obj.title);
+    return mappedArray;
 };
 
 // Do not edit below this line
