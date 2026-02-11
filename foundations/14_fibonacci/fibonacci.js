0b6124be89b547188e8e12e506906ee4205273fc
jjf5662hs6-tech <250735204+jjf5662hs6-tech@users.noreply.github.com>
2026-02-11 01:12:38 -0500

    done with this test, but i could use more time understanding it

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (typeof n === "string"){
+        n = Number(n);
+    }
+    if (n < 0){
+        return "OOPS";
+    }
+    if (n === 0){
+        return 0;
+    }
+    if (n === 1) {
+        return 1;
+    }
 
+    let prev = 0;
+    let current = 1;
+
+    for (let i = 2; i <= n; i++) {
+        let next = prev + current;
+        prev = current;
+        current = next;
+
+    }
+    return current;
 };
 
 // Do not edit below this line
