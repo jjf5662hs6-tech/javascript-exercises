b89610869b198a64d186e6b3ce6eb0c082f581f3
jjf5662hs6-tech <250735204+jjf5662hs6-tech@users.noreply.github.com>
2026-02-12 13:15:47 -0500

    finished these tests

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        const currentYear = new Date().getFullYear();
+        const oldestAge = (oldest.yearOfDeath || currentYear - oldest.yearOfBirth);
+        const personAge = (person.yearOfDeath || currentYear - person.yearOfBirth);
 
+        if (personAge > oldestAge) {
+            return person
+        } else {
+            return oldest
+        }
+    })
 };
 
 // Do not edit below this line
